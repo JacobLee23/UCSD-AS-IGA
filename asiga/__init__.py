e4a4f66a8a757a3a2d75448c27670b9850dd5ffe
Jacob Lee <JLpython@outlook.com>
2023-04-27 12:47:46 -0700

    feat: update CL argument help messages

--- asiga/__init__.py
+++ asiga/__init__.py
@@ -9,18 +9,35 @@ VERSION = metadata.version("ucsdasone")
 
 
 def _parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog="asiga",
+        description="A simple API wrapper and web scraper for the UCSD Associated Students (AS) instructor grade archive.",
+        epilog="Go Tritons!"
+    )
 
     parser.add_argument(
-        "-v", "--version", action="version",
-        version=f"{NAME} {VERSION}"
+        "-v", "--version", action="version", version=f"$(prog) {VERSION}"
     )
-    parser.add_argument("-q", "--quarter", type=str)
-    parser.add_argument("-y", "--year", type=int)
-    parser.add_argument("-i", "--instructor", type=str)
-    parser.add_argument("-s", "--subject", type=str)
-    parser.add_argument("-c", "--code", type=str)
-    parser.add_argument("-o", "--output", type=str)
+    parser.add_argument(
+        "-q", "--quarter", type=str,
+        help="filter results to the fall ('FA'), winter ('WI'), or spring ('SP')"
+    )
+    parser.add_argument(
+        "-y", "--year", type=int,
+        help=r"filter results to a specific year (r'^\d\d$')."
+    )
+    parser.add_argument(
+        "-i", "--instructor", type=str,
+        help="filter results to classes taught by a specific instructor (f'{last_name}, {first_name}')"
+    )
+    parser.add_argument(
+        "-s", "--subject", type=str,
+        help="filter results to classes in a specific department (r'^[A-Z]{3,4}$')"
+    )
+    parser.add_argument(
+        "-c", "--code", type=str,
+        help=r"filter results to a specific course number (r'^[0-9]{1,3}[A-Z]{0,2}$')"
+    )
 
     return parser
 
@@ -37,8 +54,5 @@ def main() -> None:
         code=args.code
     )
     dataframe = archive.data()
-
-    if args.output:
-        archive.export(args.output)
 
     print(dataframe.to_string())
