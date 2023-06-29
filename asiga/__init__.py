d836a48f1b38e2160b5f9ce47e2d2ced8de7c3ff
Jacob Lee <JLpython@outlook.com>
2023-06-29 13:26:49 -0700

    refactor: rename asiga.scraper

--- asiga/__init__.py
+++ asiga/__init__.py
@@ -2,12 +2,12 @@
 A simple API wrapper and web scraper for the UCSD Associated Students (AS) instructor grade archive.
 """
 
-from .scraper import GradeArchive
-from .scraper import Parser
+import _scraper
+from ._scraper import GradeArchive
 
 
 def main() -> None:
-    parser = Parser(prog="asiga", description=__doc__, epilog="Go Tritons!")
+    parser = _scraper.Parser(prog="asiga", description=__doc__, epilog="Go Tritons!")
     archive = parser.grade_archive
 
     print(archive.dataframe.to_string())
