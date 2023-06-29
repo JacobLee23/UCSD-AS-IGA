dd6a81f8bbe20268b589af8013424f1ce772c22e
Jacob Lee <JLpython@outlook.com>
2023-06-28 17:48:30 -0700

    refactor: OOP

--- asiga/__init__.py
+++ asiga/__init__.py
@@ -1,57 +1,13 @@
-import argparse
-from importlib import metadata
+"""
+A simple API wrapper and web scraper for the UCSD Associated Students (AS) instructor grade archive.
+"""
 
-from .scraper import grade_archive
-
-
-NAME = metadata.metadata("ucsdasone")["name"]
-VERSION = metadata.version("ucsdasone")
-
-
-def _parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(
-        prog="asiga",
-        description="A simple API wrapper and web scraper for the UCSD Associated Students (AS) instructor grade archive.",
-        epilog="Go Tritons!"
-    )
-
-    parser.add_argument(
-        "-v", "--version", action="version", version=f"%(prog)s {VERSION}"
-    )
-    parser.add_argument(
-        "-q", "--quarter", type=str,
-        help="filter results to the fall ('FA'), winter ('WI'), or spring ('SP')"
-    )
-    parser.add_argument(
-        "-y", "--year", type=int,
-        help=r"filter results to a specific year (r'^\d\d$')."
-    )
-    parser.add_argument(
-        "-i", "--instructor", type=str,
-        help="filter results to classes taught by a specific instructor (f'{last_name}, {first_name}')"
-    )
-    parser.add_argument(
-        "-s", "--subject", type=str,
-        help="filter results to classes in a specific department (r'^[A-Z]{3,4}$')"
-    )
-    parser.add_argument(
-        "-c", "--code", type=str,
-        help=r"filter results to a specific course number (r'^[0-9]{1,3}[A-Z]{0,2}$')"
-    )
-
-    return parser
+from .scraper import GradeArchive
+from .scraper import Parser
 
 
 def main() -> None:
-    parser = _parser()
-    args = parser.parse_args()
+    parser = Parser(prog="asiga", description=__doc__, epilog="Go Tritons!")
+    archive = parser.grade_archive
 
-    dataframe = grade_archive(
-        quarter=args.quarter,
-        year=args.year,
-        instructor=args.instructor,
-        subject=args.subject,
-        code=args.code
-    )
-
-    print(dataframe.to_string())
+    print(archive.dataframe.to_string())
--- asiga/scraper.py
+++ asiga/scraper.py
@@ -1,8 +1,9 @@
 """
 """
 
-import collections
+import argparse
 import datetime
+from importlib import metadata
 import re
 import typing
 
@@ -10,99 +11,167 @@ import pandas as pd
 import requests
 
 
-ADDRESS = "https://as.ucsd.edu/Home/InstructorGradeArchive"
-
-
-MIN_YEAR = 15
-MAX_YEAR = int(datetime.datetime.today().strftime("%y"))
-
-
-def _validate_quarter(quarter: str) -> str:
+class GradeArchive:
     """
-    :param quarter:
-    :return:
-    """
-    values = ("FA", "WI", "SP")
-    quarter = quarter.strip().upper()
+    .. py:attribute:: fields
 
-    if quarter in values:
-        return quarter
+    .. py:attribute:: quarters
 
-    return ""
+    .. py:attribute:: years
 
+    .. py:attribute:: address
 
-def _validate_year(year: int) -> str:
-    """
-    :param year:
-    :return:
-    """
-    if MIN_YEAR <= year <= MAX_YEAR:
-        return str(year)
-
-    return ""
-
-
-def _validate_subject(subject: str) -> str:
-    """
-    :param subject:
-    :return:
-    """
-    regex = re.compile(r"^[A-Z]{3,4}$")
-    subject = subject.strip().upper()
-
-    if regex.search(subject) is not None:
-        return subject
-
-    return ""
-
-
-def _validate_code(code: str) -> str:
-    """
-    :param code:
-    :return:
-    """
-    regex = re.compile(r"^\d{1,3}[A-Z]{0,2}$")
-    code = code.strip().upper()
-
-    if regex.search(code) is not None:
-        return code
-
-    return ""
-
-
-def grade_archive(
-    quarter: typing.Optional[str] = None,
-    year: typing.Optional[int] = None,
-    instructor: typing.Optional[str] = None,
-    subject: typing.Optional[str] = None,
-    code: typing.Optional[str] = None
-) -> pd.DataFrame:
-    """
     :param quarter:
     :param year:
     :param instructor:
     :param subject:
     :param code:
-    :return:
     """
-    form_data = collections.defaultdict()
+    fields = ("quarter", "year", "instructor", "subject", "code")
 
-    form_data["quarter"] = "" if quarter is None else _validate_quarter(quarter)
-    form_data["year"] = "" if year is None else _validate_year(year)
-    form_data["instructor"] = "" if instructor is None else instructor
-    form_data["subject"] = "" if subject is None else _validate_subject(subject)
-    form_data["courseNumber"] = "" if code is None else _validate_code(code)
+    quarters = ("FA", "WI", "SP")
+    years = range(15, datetime.datetime.today().year)
 
-    with requests.post(ADDRESS, form_data) as response:
-        try:
-            dfs = pd.read_html(response.text)
-        except ValueError:
-            return pd.DataFrame()
+    address = "https://as.ucsd.edu/Home/InstructorGradeArchive"
 
-    assert len(dfs) == 1
-    dataframe = dfs[0]
+    def __init__(
+        self, *, quarter: typing.Optional[str] = None, year: typing.Optional[int] = None,
+        instructor: typing.Optional[str] = None, subject: typing.Optional[str] = None,
+        code: typing.Optional[str] = None
+    ):
+        self.quarter = quarter
+        self.year = year
+        self.instructor = instructor
+        self.subject = subject
+        self.code = code
 
-    columns = ["A", "B", "C", "D", "F", "W", "P", "NP"]
-    dataframe[columns] = dataframe[columns].applymap(lambda x: float(x.strip("%")))
+    def __getitem__(self, key: str) -> typing.Optional[typing.Union[str, int]]:
+        return self.__getattribute__(key)
 
-    return dataframe
+    @property
+    def quarter(self) -> typing.Optional[str]:
+        """
+        Filters results to a specific quarter. (See :py:attr:`GradeArchive.quarters`.)
+        """
+        return self._quarter
+    
+    @quarter.setter
+    def quarter(self, value: typing.Optional[str]) -> None:
+        """
+        :param value:
+        """
+        if isinstance(value, str):
+            quarter = value.strip().upper()
+            self._quarter = quarter if quarter in self.quarters else None
+        else:
+            self._quarter = None
+    
+    @property
+    def year(self) -> typing.Optional[int]:
+        """
+        Filters results to a specific year. (See :py:attr:`GradeArchive.years`.)
+        """
+        return self._year
+    
+    @year.setter
+    def year(self, value: typing.Optional[int]) -> None:
+        """
+        :param value:
+        """
+        if isinstance(value, int):
+            self._year = value if value in self.years else None
+        else:
+            self._year = None
+
+    @property
+    def instructor(self) -> typing.Optional[str]:
+        """
+        Filters results to classes taught by a specific instructor. (Use the format "{{Last Name}}, {{First Name}}".)
+        """
+        return self._instructor
+    
+    @instructor.setter
+    def instructor(self, value: typing.Optional[str]) -> None:
+        """
+        :param value:
+        """
+        if isinstance(value, str):
+            self._instructor = value
+        else:
+            self._instructor = None
+
+    @property
+    def subject(self) -> typing.Optional[str]:
+        """
+        Filters results to classes taught in a specific department.
+        """
+        return self._subject
+    
+    @subject.setter
+    def subject(self, value: typing.Optional[str]) -> None:
+        """
+        :param value:
+        """
+        regex = re.compile(r"^[A-Z]{3,4}$")
+        if isinstance(value, str):
+            subject = value.strip().upper()
+            self._subject = None if regex.search(subject) is None else subject
+        else:
+            self._subject = None
+
+    @property
+    def code(self) -> typing.Optional[str]:
+        """
+        Filters results to a specific course number.
+        """
+        return self._code
+    
+    @code.setter
+    def code(self, value: typing.Optional[str]) -> None:
+        """
+        :param value:
+        """
+        regex = re.compile(r"^\d{1,3}[A-Z]{0,2}$")
+        if isinstance(value, str):
+            code = value.strip().upper()
+            self._code = None if regex.search(code) is None else code
+        else:
+            self._code = None
+
+    @property
+    def dataframe(self) -> pd.DataFrame:
+        """
+        """
+        data = {
+            k: ("" if self[k] is None else str(self[k])) for k in self.fields
+        }
+        with requests.post(self.address, data=data, timeout=100) as response:
+            try:
+                dataframe = pd.read_html(response.text)[0]
+            except ValueError:
+                return pd.DataFrame()
+            
+        columns = ["A", "B", "C", "D", "F", "W", "P", "NP"]
+        dataframe[columns] = dataframe[columns].applymap(lambda x: float(x.strip("%")))
+
+        return dataframe
+
+
+class Parser(argparse.ArgumentParser):
+    """
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.add_argument("-v", "--version", action="version", version=f"%(prog)s {metadata.version('ucsdasone')}")
+        self.add_argument("-q", "--quarter", type=str, help=GradeArchive.quarter.__doc__)
+        self.add_argument("-y", "--year", type=int, help=GradeArchive.year.__doc__)
+        self.add_argument("-i", "--instructor", type=str, help=GradeArchive.instructor.__doc__)
+        self.add_argument("-s", "--subject", type=str, help=GradeArchive.subject.__doc__)
+        self.add_argument("-c", "--code", type=str, help=GradeArchive.code.__doc__)
+
+    @property
+    def grade_archive(self) -> GradeArchive:
+        """
+        """
+        return GradeArchive(**self.parse_args().__dict__)
