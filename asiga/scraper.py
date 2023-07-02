1de343bf4288db107078f35c2e68dbb2be6d2147
Jacob Lee <JLpython@outlook.com>
2023-07-02 09:52:52 -0700

    chore: update dependencies

--- asiga/scraper.py
+++ asiga/scraper.py
@@ -29,7 +29,10 @@ class GradeArchive:
     """
     _url = "https://as.ucsd.edu/Home/InstructorGradeArchive"
     _headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/x-www-form-urlencoded"
     }
     _timeout = 100
 
